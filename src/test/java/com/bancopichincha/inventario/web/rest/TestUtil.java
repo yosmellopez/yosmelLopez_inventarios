5e66834d34f60bb381c6ce229dd717147be27808
Yosmel Lopez Pimentel <yosmellopez@gmail.com>
2022-04-25 13:27:34 -0500

    Feature: Se elimina la excepcion que lanza la instruccion new URI
    Se aplican las recomendaciones del IDE

--- src/test/java/com/bancopichincha/inventario/web/rest/TestUtil.java
+++ src/test/java/com/bancopichincha/inventario/web/rest/TestUtil.java
@@ -41,48 +41,6 @@ public final class TestUtil {
     }
 
     /**
-     * A matcher that tests that the examined number represents the same value - it can be Long, Double, etc - as the reference BigDecimal.
-     */
-    public static class NumberMatcher extends TypeSafeMatcher<Number> {
-
-        final BigDecimal value;
-
-        public NumberMatcher(BigDecimal value) {
-            this.value = value;
-        }
-
-        @Override
-        public void describeTo(Description description) {
-            description.appendText("a numeric value is ").appendValue(value);
-        }
-
-        @Override
-        protected boolean matchesSafely(Number item) {
-            BigDecimal bigDecimal = asDecimal(item);
-            return bigDecimal != null && value.compareTo(bigDecimal) == 0;
-        }
-
-        private static BigDecimal asDecimal(Number item) {
-            if (item == null) {
-                return null;
-            }
-            if (item instanceof BigDecimal) {
-                return (BigDecimal) item;
-            } else if (item instanceof Long) {
-                return BigDecimal.valueOf((Long) item);
-            } else if (item instanceof Integer) {
-                return BigDecimal.valueOf((Integer) item);
-            } else if (item instanceof Double) {
-                return BigDecimal.valueOf((Double) item);
-            } else if (item instanceof Float) {
-                return BigDecimal.valueOf((Float) item);
-            } else {
-                return BigDecimal.valueOf(item.doubleValue());
-            }
-        }
-    }
-
-    /**
      * Verifies the equals/hashcode contract on the domain object.
      */
     public static <T> void equalsVerifier(Class<T> clazz) throws Exception {
